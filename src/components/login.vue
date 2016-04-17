7d385abcf0e3649e3594a48e4153a4616dac9041
wynfrith <wangfucheng56@gmail.com>
2016-04-17 17:32:46 +0800

    添加vue-form验证; 完成登录页表单验证

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,4 +1,84 @@
+<script>
+  export default {
+    data() {
+      return {
+        myform: {},
+        username: '', // or email
+        password: ''
+      }
+    },
+    methods: {
+      onSubmit() {
+        if(this.myform.$valid==true)
+          alert("提交成功");
+      }
+    }
+  }
+
+</script>
+
+<template>
+  <div class="ui middle aligned center aligned grid">
+    <div class="column">
+      <h2 class="ui green image header">用户登录</h2>
+      <form v-form name="myform" class="ui large form" v-on:submit.prevent="onSubmit" >
+        <div class="ui stacked segment">
+          <div class="errors" v-if="myform.$submitted">
+            <p v-if="myform.username.$error.required">请输入用户名或邮箱</p>
+            <p v-if="myform.password.$error.required">请输入密码</p>
+          </div>
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input
+                type="text"
+                name="username"
+                v-model="username"
+                v-form-ctrl
+                required
+                placeholder="用户名或邮箱">
+            </div>
+          </div>
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="lock icon"></i>
+              <input
+                type="password"
+                name="password"
+                v-model="password"
+                v-form-ctrl
+                required
+                placeholder="输入密码">
+            </div>
+          </div>
+
+
+          <button type="submit" class="ui fluid green submit button" >登录</button>
+          <a class="ui fluid button" v-link="{name: 'signin'}">我是新用户</a>
+          <div class="forget">
+            <a class="ui basic teal label" v-link="{name: 'forget'}">忘记密码?</a>
+          </div>
+          <div class="clear"></div>
+        </div>
+
+
+
+      </form>
+    </div>
+  </div>
+
+</template>
+
 <style scoped media="screen">
+    .errors {
+      font-size: .9rem;
+      padding: 10px 0;
+    }
+    .errors p{
+      background-color: #FFACA6;
+      margin: 0 0 .5rem;
+      padding: 2px;
+    }
     .column {
       margin-top: 60px;
       max-width: 450px;
@@ -23,37 +103,3 @@
     }
 
 </style>
-<template>
-  <div class="ui middle aligned center aligned grid">
-    <div class="column">
-      <h2 class="ui green image header">用户登录</h2>
-      <form class="ui large form">
-        <div class="ui stacked segment">
-          <div class="field">
-            <div class="ui left icon input">
-              <i class="user icon"></i>
-              <input type="text" name="email" placeholder="用户名或邮箱">
-            </div>
-          </div>
-          <div class="field">
-            <div class="ui left icon input">
-              <i class="lock icon"></i>
-              <input type="password" name="password" placeholder="输入密码">
-            </div>
-          </div>
-          <button class="ui fluid green submit button">登录</button>
-          <a class="ui fluid button" v-link="{name: 'signin'}">我是新用户</a>
-          <div class="forget">
-            <a class="ui basic teal label" v-link="{name: 'forget'}">忘记密码?</a>
-          </div>
-          <div class="clear"></div>
-        </div>
-
-        <div class="ui error message"></div>
-
-      </form>
-
-    </div>
-  </div>
-
-</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueForm from 'vue-form'
 import VueResource from 'vue-resource'
+
+// npm引用需要 use加载
+Vue.use(VueForm)
 
 import App from './App.vue'
 import routerMap from './router'
