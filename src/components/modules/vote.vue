75101e3936c60fdbf160e61b941f803b80c5f754
wynfrith <wangfucheng56@gmail.com>
2016-04-19 21:47:35 +0800

    投票组件逻辑修改和美化

--- src/components/modules/vote.vue
+++ src/components/modules/vote.vue
@@ -5,7 +5,8 @@
     },
     data() {
       return {
-        voteStatus: 0
+        voteStatus: 0,
+        msg: ''
       }
     },
     methods: {
@@ -14,23 +15,21 @@
           if (this.voteStatus == 1) { // undo like
             this.voteStatus = 0;
             this.counts -= 1;
-          } else if (this.voteStatus == -1){ // like and cancel hate
-            this.voteStatus = 1;
-            this.counts += 2;
-          } else{ // like
-            this.voteStatus = 1;
+            this.$dispatch('msg', { type: 'ok', text: '您取消了点赞'});
+          } else { // like
+            this.voteStatus += 1;
             this.counts += 1;
+            this.$dispatch('msg', { type: 'ok', text:'点赞成功!'});
           }
         } else if(num == -1) {
           if (this.voteStatus == -1) {
             this.voteStatus = 0; // undo hate
             this.counts += 1;
-          } else if (this.voteStatus == 1){ // hate and cancel like
-            this.voteStatus = -1;
-            this.counts -= 2;
+            this.$dispatch('msg', { type: 'ok', text:'您取消了踩'});
           } else {
-            this.voteStatus = -1;
+            this.voteStatus -= 1;
             this.counts -= 1;
+            this.$dispatch('msg', { type: 'ok', text:'您踩了一下..'});
           }
         }
       }
@@ -42,8 +41,8 @@
   <div class="vote">
     <button
       type="button"
-      class="ui basic button up"
-      :class="{'blue': voteStatus == 1}"
+      class="ui button up"
+      :class="{'green': voteStatus == 1}"
       @click="vote(1)"
     ><i class="caret up icon"></i></button>
 
@@ -51,7 +50,7 @@
 
     <button
       type="button"
-      class="ui basic button down"
+      class="ui button down"
       :class="{'red': voteStatus == -1}"
       @click="vote(-1)"
     ><i class="caret down icon"></i></button>
@@ -94,4 +93,28 @@
     padding-top: 26px;
     color: rgba(0,0,0,.6);
   }
+  .button.green {
+    /*color:#21ba45*/
+  }
+  .ui.button:focus {
+    background-color: #e0e1e2;
+    color: rgba(0,0,0,.6);
+  }
+  .ui.green.button:focus {
+    background-color: #21ba45;
+    color: #fff;
+  }
+  .button.red {
+    background-color: rgb(185, 118, 97);
+  }
+  .ui.red.button:focus {
+    background-color: rgb(185, 118, 97);
+    color: #fff;
+  }
+  .ui.red.button:hover {
+    background-color: #B56B54;
+    color: #fff;
+  }
+
+
 </style>
